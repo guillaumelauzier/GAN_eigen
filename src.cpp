f46a24cb7fdfbdde9422f75f71d6bc1ce9a16a74
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2022-12-15 02:57:04 +0100

    Update src.cpp

--- src.cpp
+++ src.cpp
@@ -65,3 +65,29 @@ MatrixXd generator_gradients = -d_on_g.array() / generated.array();
 
 // Update the discriminator network
 MatrixXd real = Matrix
+X.block(0, 0, real_images.rows, real_images.cols);
+MatrixXd d_on_real = discriminator(real);
+MatrixXd d_on_fake = discriminator(generated);
+
+// Compute the loss and gradients for the discriminator network
+double discriminator_loss = -(d_on_real.array().log() + (1.0 - d_on_fake.array()).log()).sum();
+MatrixXd d_real_gradients = -d_on_real.array() / real.array();
+MatrixXd d_fake_gradients = d_on_fake.array() / generated.array();
+MatrixXd discriminator_gradients = d_real_gradients + d_fake_gradients;
+
+// Update the discriminator network weights and biases
+// (omitted for brevity)
+
+// Print the current loss values
+std::cout << "Generator loss: " << generator_loss << std::endl;
+std::cout << "Discriminator loss: " << discriminator_loss << std::endl;
+
+// Show the generated images
+imshow("Generated", generated);
+waitKey(1);
+}
+
+return 0;
+}
+
+
